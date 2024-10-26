49e23916d26bde3e33b3a461bf94ec90b9ee93f2
Weerawat Seesoda <weerawat.seesoda@gmail.com>
2024-10-27 02:39:47 +0700

    Bump up version number to 0.24.10.3

--- src/version.cpp
+++ src/version.cpp
@@ -2,5 +2,5 @@
 #include <iostream>
 
 void showVersion() {
-	std::cout << "Subnautica Resource Calculator version: 0.24.10.2\n";
+	std::cout << "Subnautica Resource Calculator version: 0.24.10.3\n";
 }
