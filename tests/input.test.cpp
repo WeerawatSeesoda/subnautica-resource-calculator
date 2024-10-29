7663ace3add3e837b701f3fbcf8f4a05ef109e2e
Weerawat Seesoda <weerawat.seesoda@gmail.com>
2024-10-29 22:45:44 +0700

    Refactoring: Rename some tests to more accurate

--- tests/input.test.cpp
+++ tests/input.test.cpp
@@ -1,49 +1,49 @@
 #include "../src/input.hpp"
 #include <gtest/gtest.h>
 
-TEST(Input_Validation, Zero) {
+TEST(Manual_Input_to_Number, Zero) {
 	int expected = 0;
 	int result = convertStringToNumber("0");
 
 	EXPECT_EQ(expected, result);
 }
 
-TEST(Input_Validation, One) {
+TEST(Manual_Input_to_Number, One) {
 	int expected = 1;
 	int result = convertStringToNumber("1");
 
 	EXPECT_EQ(expected, result);
 }
 
-TEST(Input_Validation, Two) {
+TEST(Manual_Input_to_Number, Two) {
 	int expected = 2;
 	int result = convertStringToNumber("2");
 
 	EXPECT_EQ(expected, result);
 }
 
-TEST(Input_Validation, Two_Digit) {
+TEST(Manual_Input_to_Number, Two_Digit) {
 	int expected = 69;
 	int result = convertStringToNumber("69");
 
 	EXPECT_EQ(expected, result);
 }
 
-TEST(Input_Validation, Negative_Number) {
+TEST(Manual_Input_to_Number, Negative_Number) {
 	int expected = -69;
 	int result = convertStringToNumber("-69");
 
 	EXPECT_EQ(expected, result);
 }
 
-TEST(Input_Validation, Not_Number) {
+TEST(Manual_Input_to_Number, Not_Number) {
 	int expected = -1;
 	int result = convertStringToNumber("a");
 
 	EXPECT_EQ(expected, result);
 }
 
-TEST(Input_Validation, Mix_Number_and_Alphabets) {
+TEST(Manual_Input_to_Number, Mix_Number_and_Alphabets) {
 	int expected = -1;
 
 	int alphabet_then_number = convertStringToNumber("a69");
@@ -58,3 +58,4 @@ TEST(Input_Validation, Mix_Number_and_Alphabets) {
 	int alphabet_between_numbers = convertStringToNumber("64a420");
 	EXPECT_EQ(expected, alphabet_between_numbers);
 }
+
