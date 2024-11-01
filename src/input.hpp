6d21143a15c612fa790ddb25df14b676b8c4d938
Weerawat Seesoda <weerawat.seesoda@gmail.com>
2024-11-01 19:37:56 +0700

    Refactoring: Edit a comment

--- src/input.hpp
+++ src/input.hpp
@@ -3,9 +3,10 @@
 
 name_amount_pair keyboardInput();
 
-/** Convert string to number if the string contain only an integer number.
- * Return the number  -  If only contain an integer number.
- * Return  0          -  If legit as above and is "-1".
- * Return -1          -  Otherwise.
+/**
+ * Convert string to integer if the string contain only integer.
+ * @return : The number, If success convert.
+ * @return : 0, If success convert but the input is "-1".
+ * @return : -1, If error.
  */
 int convertStringToNumber(const char* str);
